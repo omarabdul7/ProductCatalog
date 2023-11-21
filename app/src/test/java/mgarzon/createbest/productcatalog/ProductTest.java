d4c91a706ab681451156c4959865d3b516de1627
Omar Abdul <omaraabdul9@gmail.com>
2023-11-21 15:24:48 -0500

    lab8

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -18,10 +18,10 @@ public class ProductTest {
 
     @Test
     public void checkProductPrice() {
-        // I am making this test to fail
         Product aProduct = new Product("1", "DELL MONITOR", 180);
-        assertEquals("Check the price of the product", "0", aProduct.getPrice());
+        assertEquals("Check the price of the product", 180, aProduct.getPrice(), 0);
     }
+
 
 
 }
